9e732c066c174f0c581d59a72a8227218a1c813a
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-02-17 19:28:23 +0900

    :sparkles: Introduce authorization request

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,4 +1,32 @@
 class AuthorizationController < ApplicationController
   def index
+    @client = Client.find_by(uid: params[:client_id])
+
+    if @client.nil?
+      raise "client_id is wrong."
+    end
+
+    redirect_uri = params[:redirect_uri]
+
+    if redirect_uri.nil? || @client.redirect_uri != redirect_uri
+      head :bad_request
+    end
+
+    if params['response_type'] != 'code'
+      head :bad_request
+    end
+
+    state = params['state'].nil? ? '' : params['state']
+
+    accept_scopes = Scope.all.map(&:name)
+
+    @scopes = params[:scope].split(/\s+/).find_all { |scope| accept_scopes.include?(scope) }
+
+    session[:client]       = @client
+    session[:state]        = state
+    session[:scopes]       = @scopes
+    session[:redirect_uri] = redirect_uri
   end
 end
+
+
